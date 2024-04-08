9c95aecc19b79bb94bc1448a73ce305e27ec3a5e
NAILAinfo <165082374+NAILAinfo@users.noreply.github.com>
2024-04-08 23:40:40 +0200

    Update Date.java

--- Date.java
+++ Date.java
@@ -5,18 +5,10 @@ public class Date {
     private int annee;
 //constructeur
     public Date(int jour, int mois, int annee) {
-      
-        if (mois < 1 || mois > 12) {
-            throw new IllegalArgumentException("Le mois doit être compris entre 1 et 12.");
-        }
-        if (jour < 1 || jour > 31) {
-            throw new IllegalArgumentException("Le jour doit être compris entre 1 et 31.");
-        }
-
-        if ((mois == 4 || mois == 6 || mois == 9 || mois == 11) && jour > 30) {
-            throw new IllegalArgumentException("Le mois " + mois + " n'a que 30 jours.");
-        }
-
+        if (mois < 1 || mois > 12) {throw new IllegalArgumentException("Le mois doit être compris entre 1 et 12."); }
+        if (jour < 1 || jour > 31) {throw new IllegalArgumentException("Le jour doit être compris entre 1 et 31."); }
+        if ((mois == 4 || mois == 6 || mois == 9 || mois == 11) && jour > 30) {throw new IllegalArgumentException("Le mois " + mois + " n'a que 30 jours.");}
+// Déterminer le nombre de jours du deuxième mois (février) en effectuant des calculs sur l'année pour déterminer si elle est bissextile
         if (mois == 2) {
             if ((annee % 4 == 0 && annee % 100 != 0) || annee % 400 == 0) {
                 if (jour > 29) {
@@ -28,7 +20,6 @@ public class Date {
                 }
             }
         }
-
         this.jour = jour;
         this.mois = mois;
         this.annee = annee;
