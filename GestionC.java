9b50b216b093b8c50878225ebc4e411daaeb93e8
NAILAinfo <165082374+NAILAinfo@users.noreply.github.com>
2024-04-09 19:57:39 +0200

    Create GestionC.java

---
+++ GestionC.java
@@ -0,0 +1,46 @@
+import java.util.HashMap;
+import java.util.Map;
+// a modifier
+public class GestionC {
+
+    private Map<String, Chambre> chambres;
+
+    public GestionC() {
+        chambres = new HashMap<>();
+    }
+
+    public Map<String, Chambre> getChambres() {
+        return chambres;
+    }
+
+    public void setChambres(Map<String, Chambre> chambres) {
+        this.chambres = chambres;
+    }
+
+    public void modifierChambre(String numeroChambre, Chambre nouvelleChambre) {
+        if (chambres.containsKey(numeroChambre)) {
+            chambres.put(numeroChambre, nouvelleChambre);
+            System.out.println("La chambre a été modifiée avec succès.");
+        } else {
+            System.out.println("La chambre avec le numéro " + numeroChambre + " n'existe pas.");
+        }
+    }
+
+   public void supprimerChambre(String numeroChambre) {
+        if (chambres.containsKey(numeroChambre)) {
+            chambres.remove(numeroChambre);
+            System.out.println("La chambre a été supprimée avec succès.");
+        } else {
+            System.out.println("La chambre avec le numéro " + numeroChambre + " n'existe pas.");
+        }
+    }
+
+    public void ajouterChambre(Chambre chambre) {
+        chambres.put(chambre.getNumero(), chambre);
+    }
+
+    public Chambre trouverChambreParNumero(String numeroChambre) {
+        return chambres.get(numeroChambre);
+    }
+
+}
