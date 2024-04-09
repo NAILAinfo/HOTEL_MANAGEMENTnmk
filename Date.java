c9b8284765afe55f6f869ec8835925bd234b30d5
NAILAinfo <165082374+NAILAinfo@users.noreply.github.com>
2024-04-09 15:55:11 +0200

    Update Date.java

--- Date.java
+++ Date.java
@@ -3,14 +3,36 @@ public class Date {
     private int jour;
     private int mois;
     private int annee;
+// determiner si le mois a 30 jours 
+    public boolean mois30j(){
+        return (mois == 4 || mois == 6 || mois == 9 || mois == 11) ;
+    }
+// determiner si le mois a 31 jours 
+    public boolean mois31j(){
+        return (mois == 1 || mois == 3 || mois == 5 || mois == 7|| mois == 8 || mois == 10|| mois == 12);
+    }
+// determiner si l'annee est bissextile
+    public boolean anneebissextile(){
+        return (annee % 4 == 0 && annee % 100 != 0) || annee % 400 == 0;
+    }
+// determiner le nombre de jour du mois 
+    public int nbrjours(int m ){
+       if (m == 2) {return anneebissextile() ? 29 : 28;} 
+       else if (mois30j()) {return 30;} 
+       else {return 31; } 
+    }
+    
+    
 //constructeur
     public Date(int jour, int mois, int annee) {
         if (mois < 1 || mois > 12) {throw new IllegalArgumentException("Le mois doit être compris entre 1 et 12."); }
         if (jour < 1 || jour > 31) {throw new IllegalArgumentException("Le jour doit être compris entre 1 et 31."); }
-        if ((mois == 4 || mois == 6 || mois == 9 || mois == 11) && jour > 30) {throw new IllegalArgumentException("Le mois " + mois + " n'a que 30 jours.");}
+        
+        if (mois30j() && jour > 30) {throw new IllegalArgumentException("Le mois " + mois + " n'a que 30 jours.");}
+        
 // Déterminer le nombre de jours du deuxième mois (février) en effectuant des calculs sur l'année pour déterminer si elle est bissextile
         if (mois == 2) {
-            if ((annee % 4 == 0 && annee % 100 != 0) || annee % 400 == 0) {
+            if (anneebissextile()) {
                 if (jour > 29) {
                     throw new IllegalArgumentException("Février en année bissextile a 29 jours.");
                 }
@@ -33,11 +55,26 @@ public class Date {
     public void setMois(int mois) {this.mois = mois;}
     public void setAnnee(int annee) {this.annee = annee;}
 
-    public String toString() {
-        return "Date{" +"jour=" + jour +", mois=" + mois +", annee=" + annee +'}';
-    }
-
-        // Creation d'une date 
+    public String toString() {return "Date{" +"jour=" + jour +", mois=" + mois +", annee=" + annee +'}';}
+    public boolean egale(Date autre){return jour==autre.jour && mois==autre.mois &&annee==autre.annee ;}
+    
+ public Date hier (){
+     int j=jour;
+     int m=mois;
+     int a=annee;
+     // il y a 3 cas :
+     //1/1/a resultat : 31/12/a--
+     // 1/**/**** resultat : dernier jour du mois precedent/m--/a
+      // la date n'est pas le premier jour du mois resultat : j--/m/a
+     if (j == 1) {
+            if (m == 1) {j = 31;m = 12;a--;}
+            else {
+                m--;j = nbrjours(m);
+            }
+        } else {j--;}
+return new Date(j,m,a);
+}
+    // Creation d'une date 
         //try {
           //  Date date = new Date(30, 2, 2024);
       //  } catch (IllegalArgumentException e) {
