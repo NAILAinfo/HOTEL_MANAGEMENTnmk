1d3a5053c8b73f65177a4b7addc43672c673a5a8
Anissa11 <162956145+Anissa11@users.noreply.github.com>
2024-05-01 02:39:12 +0200

    Update Client.java

--- Client.java
+++ Client.java
@@ -1,8 +1,13 @@
-//exception pour ne pas donner une adresse email fausse 
+
+import java.util.ArrayList;
+import java.util.List;
+
+//exception pour ne pas donner une adresse email fausse
 class EmailFormatException extends Exception {
     public EmailFormatException(String message) {
         super(message);
-}}
+    }}
+
 public class Client {
     private String nom;
     private String prenom;
@@ -10,9 +15,12 @@ public class Client {
     private String email;
     private String motDePasse;
     private String tel;
-    private Date naissance;
-//constructeur dans le cas d'inscription avec l'adresse email 
-    public Client(String nom, String prenom, String num, String email, String motDePasse, String tel, Date naissance) throws EmailFormatException {
+    private MyDate naissance;
+    private List<Reservation> demandesReservation;
+    private List<Reservation> reservations;
+
+    //constructeur dans le cas d'inscription avec l'adresse email
+    public Client(String nom, String prenom, String num, String email, String motDePasse, String tel, MyDate naissance) throws EmailFormatException {
         if (tel.length() < 10 ) {throw new IllegalArgumentException("Le numéro de téléphone est incomplet.");}
         if (tel.charAt(0) != '0' ) {throw new IllegalArgumentException("Le numéro de téléphone doit commencer par zéro !");}
         String standard = "^[a-zA-Z0-9]+@gmail\\.com$";
@@ -25,9 +33,11 @@ public class Client {
         this.motDePasse = motDePasse;
         this.tel = tel;
         this.naissance = naissance;
+        this.demandesReservation=new ArrayList <>();
+        this.reservations = new ArrayList<>();
     }
-//constructeur dans le cas d'inscription avec le numéro de téléphone 
-    public Client(String nom, String prenom, String num, String tel, Date naissance)  throws EmailFormatException {
+    //constructeur dans le cas d'inscription avec le numéro de téléphone
+    public Client(String nom, String prenom, String num, String tel, MyDate naissance)  throws EmailFormatException {
         if (tel.length() < 10 ) {throw new IllegalArgumentException("Le numéro de téléphone est incomplet.");}
         if (tel.charAt(0) != '0' ) {throw new IllegalArgumentException("Le numéro de téléphone doit commencer par zéro !");}
         this.nom = nom;
@@ -37,22 +47,68 @@ public class Client {
         this.motDePasse = null;
         this.tel = tel;
         this.naissance = naissance;
+        this.demandesReservation=new ArrayList <> ();
+        this.reservations = new ArrayList<>();
     }
-//getters
+    //getters
     public String getNom() {return nom;}
     public String getPrenom() {return prenom;}
     public String getNum() { return num;}
     public String getEmail() {return email;}
     public String getMotDePasse() {return motDePasse;}
     public String getTel() {return tel;}
-    public Date getNaissance() {return naissance;}    
- //setters   
+    public MyDate getNaissance() {return naissance;}
+    //setters
     public void setNom(String nom) { this.nom = nom;}
     public void setPrenom(String prenom) {this.prenom = prenom;}
     public void setNum(String num) {this.num = num;}
     public void setEmail(String email) {this.email = email;}
     public void setMotDePasse(String motDePasse) {this.motDePasse = motDePasse;}
     public void setTel(String tel) {this.tel = tel;}
-    public void setNaissance(Date naissance) {this.naissance = naissance;}
+    public void setNaissance(MyDate naissance) {this.naissance = naissance;}
+
+    // Méthode pour faire une demande de réservation
+    public void demanderReservation(Chambre chambre, MyDate Debut, MyDate Fin) {
+        // Vérifier la disponibilité de la chambre pour les dates spécifiées
+        if (chambre.estDisponible(Debut, Fin)) {
+            // Créer une nouvelle demande de réservation
+            Reservation nouvelleDemande = new Reservation(chambre, Debut, Fin);
+            // Ajouter la demande à la liste des demandes de réservation
+
+            reservations.add(nouvelleDemande);
+            // Afficher un message de confirmation
+            System.out.println("Demande de réservation effectuée avec succès pour la chambre " + chambre.getNumero() + ".");
+        } else {
+            // Afficher un message d'erreur si la chambre n'est pas disponible
+            System.out.println("La chambre " + chambre.getNumero() + " n'est pas disponible pour les dates spécifiées.");
+        }
+    }
+
+    //creer Reservation
+    public void creerReservation(Chambre chambre, Client client, MyDate dateDebut, MyDate dateFin, String statut) {
+        // Créer une nouvelle réservation avec les informations spécifiées
+        Reservation nouvelleReservation = new Reservation(chambre , dateDebut,dateFin);
+        // Appeler la méthode reserver de la réservation pour effectuer la réservation
+        nouvelleReservation.reserver(dateDebut, dateFin);
+        // Ajouter la réservation à la liste des réservations du client
+        reservations.add(nouvelleReservation);
+    }
+
+    public List<Reservation> getReservations() {
+        return reservations;
+    }
+
+    //afficher la liste des reservation d'un client specifique
+    public void afficherReservations(Client client) {
+        System.out.println("Liste des réservations pour le client " + client.getNom() + " " + client.getPrenom() + ":");
+        for (Reservation reservation : client.getReservations()) {
+            System.out.println("Chambre: " + reservation.getChambre().getNumero() +
+                    ", MyDate de début: " + reservation.getDebut() +
+                    ", MyDate de fin: " + reservation.getFin());
+        }
+    }
+
+
+
 }
 
